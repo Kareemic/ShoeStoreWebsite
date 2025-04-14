88f9b8410bbcb10d7fb7863ddd992cf6d3e2bca6
Karim Krklec <129008309+Kareemic@users.noreply.github.com>
2025-04-14 21:21:41 +0200

    FINAL

--- data.js
+++ data.js
@@ -1,3 +1,6 @@
+
+
+
 const data = {
     "website": "Shoes",
     "categories": [
@@ -16,20 +19,20 @@ const data = {
                 }
                 ,                
                 { 
-                    "name" : "Yeezy 350V2 Oreo", "image" : "./Pictures/Yeezy/yeezy350 blackorange.jpg","price":"149.99"
+                    "name" : "Yeezy 350V2 Zebra", "image" : "./Pictures/Yeezy/zebra.png","price":"149.99"
                 }
                 ,                
                 { 
-                    "name" : "Yeezy 350V2 Oreo", "image" : "./Pictures/Yeezy/yeezy350 blackorange.jpg","price":"149.99"
+                    "name" : "Yeezy 350V2 Beige", "image" : "./Pictures/Yeezy/bez.jpeg","price":"149.99"
                 }
             ]
         },
         { 
             "name" : "Adidas",
-            "image" : "candy.jpg",
+            "image" : "Adidas.jpg",
             "products" : [
                 { 
-                    "name" : "Adidas B300 black", "image" : "./Pictures/Adidas/black.jpeg","price":"79.99"
+                    "name" : "Adidas B300 Black", "image" : "./Pictures/Adidas/black.jpeg","price":"79.99"
                 },
                 { 
                     "name" : "Adidas B300 Red", "image" : "./Pictures/Adidas/red.jpeg","price":"79.99"
@@ -37,6 +40,214 @@ const data = {
                 ,
                 { 
                     "name" : "Adidas Asin Black", "image" : "./Pictures/Adidas/black2.jpeg","price":"99.99"
+                }
+                ,
+                { 
+                    "name" : "Adidas A100 Blue", "image" : "./Pictures/Adidas/blue.jpeg","price":"139.99"
+                }
+                ,
+                { 
+                    "name" : "Adidas Stan Smith", "image" : "./Pictures/Adidas/white.jpeg","price":"69.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Nike",
+            "image" : "Nike.jpg",
+            "products" : [
+                { 
+                    "name" : "Nike Dunk Low Red", "image" : "./Pictures/Nike/red.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Nike Dunk Blue", "image" : "./Pictures/Nike/blue.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Nike Dunk High Gray", "image" : "./Pictures/Nike/gray.jpeg","price":"99.99"
+                }
+                ,
+                { 
+                    "name" : "Nike Dunk Orange", "image" : "./Pictures/Nike/orange.jpeg","price":"139.99"
+                }
+                ,
+                { 
+                    "name" : "Nike Air Fast", "image" : "./Pictures/Nike/bez.jpeg","price":"69.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Off White",
+            "image" : "Off White.jpg",
+            "products" : [
+                { 
+                    "name" : "Off White Black ", "image" : "./Pictures/Off White/black.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Off White Green ", "image" : "./Pictures/Off White/green.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Off White White ", "image" : "./Pictures/Off White/white.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Off White Pink ", "image" : "./Pictures/Off White/pink.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Off White Black ", "image" : "./Pictures/Off White/black.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "McQueen",
+            "image" : "Mcqueen.jpg",
+            "products" : [
+                { 
+                    "name" : "Mcqueen blue " , "image" : "./Pictures/McQueen/blue.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Mcqueen black ", "image" : "./Pictures/McQueen/black.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Mcqueen Pink ", "image" : "./Pictures/McQueen/pink.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Mcqueen White ", "image" : "./Pictures/McQueen/white.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Mcqueen Gold ", "image" :"./Pictures/McQueen/gold.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "New Balance",
+            "image" : "New Balance.jpg",
+            "products" : [
+                { 
+                    "name" : "New Balance black Ego" , "image" : "./Pictures/New Balance/black.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "New Balance black 530", "image" : "./Pictures/New Balance/black2.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "New Balance blue 530", "image" : "./Pictures/New Balance/blue.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "New Balance blue Async ", "image" : "./Pictures/New Balance/blue2.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Mcqueen Gold ", "image" :"./Pictures/New Balance/black.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Air Jordan",
+            "image" : "New Balance.jpg",
+            "products" : [
+                { 
+                    "name" : "Air Jordan Black" , "image" : "./Pictures/Air Jordan/black.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Air Jordan White", "image" : "./Pictures/Air Jordan/white.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Air Jordan Brown", "image" : "./Pictures/Air Jordan/brown.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Air Jordan Red", "image" : "./Pictures/Air Jordan/red.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Air Jordan Blue", "image" :"./Pictures/Air Jordan/blue.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Hoka",
+            "image" : "Hoka.jpg",
+            "products" : [
+                { 
+                    "name" : "Hoka White" , "image" : "./Pictures/Hoka/white.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Hoka Gray", "image" : "./Pictures/Hoka/gray.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Hoka Pink", "image" : "./Pictures/Hoka/pink.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Hoka Orange", "image" : "./Pictures/Hoka/orange.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Hoka Black", "image" :"./Pictures/Hoka/black.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Converse",
+            "image" : "Converse.jpg",
+            "products" : [
+                { 
+                    "name" : "Converse All Star White Low" , "image" : "./Pictures/Converse/white.jpeg","price":"79.99"
+                },
+                { 
+                    "name" : "Converse All Star Black Low", "image" : "./Pictures/Converse/black low.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Converse All Star Black High", "image" : "./Pictures/Converse/black high.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Converse All Star Beige Low", "image" : "./Pictures/Converse/bez low.jpeg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Converse All Star Beige High", "image" :"./Pictures/Converse/bez high.jpeg","price":"79.99"
+                }
+            ]
+        }
+        ,
+        { 
+            "name" : "Loewe",
+            "image" : "Hoka.jpg",
+            "products" : [
+                { 
+                    "name" : "Loewe White" , "image" : "./Pictures/Loewe/white.jpg","price":"79.99"
+                },
+                { 
+                    "name" : "Loewe Gray", "image" : "./Pictures/Loewe/gray.jpg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Loewe Green", "image" : "./Pictures/Loewe/green.jpg","price":"79.99"
+                }
+                ,
+                { 
+                    "name" : "Loewe Blue", "image" : "./Pictures/Loewe/blue.jpg","price":"79.99"
+                }
+                ,
+                { 
+                   "name" : "Loewe Black", "image" :"./Pictures/Loewe/black.jpg","price":"79.99"
                 }
             ]
         }
@@ -91,14 +302,22 @@ elementiKat.forEach(item=>{item.addEventListener("click",function(){
 console.log(elementiKat);
 
 
-const cartItems =[];
+const cartItems = JSON.parse(sessionStorage.getItem('cartItems')) || [];
+if(cartItems.length!=0){
+    document.documentElement.style.setProperty("--show-cart",1);
+    document.documentElement.style.setProperty("--cart-count",`"${cartItems.length}"`);
+}
 const Buttons=document.querySelectorAll(".addToCart");
 const cart=document.getElementById("kosarica");
 
+
+
 function addToCart(product){
     cartItems.push(product);
+    sessionStorage.setItem('cartItems',JSON.stringify(cartItems));
     console.log(product);
     updateCart();
+    
 
 }
 
